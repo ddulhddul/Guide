b5dc34b504848745ac73c47814895d01314d8737
ddulh <rjw88@naver.com>
2017-07-05 16:51:06 +0900

    Strategy complete

--- be_Strategy2/src/Main.java
+++ be_Strategy2/src/Main.java
@@ -2,17 +2,20 @@ public class Main{
 	public static void main(String[] args){
 		SearchEngine cse=null;
 
-		cse=new SearchEngine("Quick");
+//		cse=new SearchEngine("Quick");
+		cse=new SearchEngine(new Quick());
 		cse.search();
 		
 		System.out.println();
 		
-		cse.setSort("Merge");
+//		cse.setSort("Merge");
+		cse.setStrategy(new Merge());
 		cse.search();
 		
 		System.out.println();
 		
-		cse=new SearchEngine("Bubble");
+//		cse=new SearchEngine("Bubble");
+		cse=new SearchEngine(new Bubble());
 		cse.search();
 	}
 }
--- be_Strategy2/src/SearchEngine.java
+++ be_Strategy2/src/SearchEngine.java
@@ -2,28 +2,32 @@ import java.util.*;
 
 public class SearchEngine {
 	private String sort;
+	private Strategy strategy;
 
 	public SearchEngine(String sort) {
 		this.sort = sort;
 	}
+	public SearchEngine(Strategy strategy) {
+		this.strategy= strategy;
+	}
 	public List<Object> search() {
 		List<Object> list = new ArrayList<Object>(); //perform search
 		
+		strategy.sort(list);
+		/*
 		if(sort.equals("Quick")){
-			// quick sort logic
-			System.out.println("Quick Sort");
 		}else if(sort.equals("Merge")){
-			// merge sort logic
-			System.out.println("Merge Sort");
 		}else if(sort.equals("Bubble")){
-			// bubble sort logic
-			System.out.println("Bubble Sort");
 		}
+		*/
 		
 		return list;
 	}
 	public void setSort(String sort) {
 		this.sort = sort;
+	}
+	public void setStrategy(Strategy strategy) {
+		this.strategy= strategy;
 	}
 }
 
