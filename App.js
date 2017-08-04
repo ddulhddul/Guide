7bb5094eca0eb78fefcb56db32e5415a3813347e
unknown <rjw88@naver.com>
2017-08-04 19:43:31 +0900

    init flex (navBar, Body)

--- App.js
+++ App.js
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!!!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.app}>
+        <NavBar />
+        <Body />
       </View>
     );
   }
 }
 
+class NavBar extends Component {
+  render() {
+    return (
+      <View style={styles.navBar}>
+        <Text>NavBar</Text>
+      </View>
+    );
+  }
+}
+
+class Body extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.left}></View>
+        <View style={styles.right}>
+          <View style={styles.top}></View>
+          <View style={styles.bottom}></View>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+
+
 const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    flexDirection: 'column'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'row'
+  },
+  left: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  right: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  top: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  bottom: {
+    flex: 2,
+    backgroundColor: 'green'
+  },
+  navBar: {
+    height: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
--- README.md
+++ README.md
@@ -8,3 +8,36 @@
 
 ## expo
 [https://expo.io/](https://expo.io/)
+
+
+## Reactjs code Snippets (vscode)
+Below is a list of all available snippets and the triggers of each one. The ⇥ means the TAB key.
+
+Trigger	|Content
+-|-
+rcc→	|class component skeleton
+rccp→	|class component skeleton with prop types after the class
+rcjc→	|class component skeleton without import and default export lines
+rcfc→	|class component skeleton that contains all the lifecycle methods
+rwwd→	|class component without import statements
+rsc→	|stateless component skeleton
+rscp→	|stateless component with prop types skeleton
+rpt→	|empty propTypes declaration
+con→	|class default constructor with props
+conc→	|class default constructor with props and context
+est→	|empty state object
+cwm→	|componentWillMount method
+cdm→	|componentDidMount method
+cwr→	|componentWillReceiveProps method
+scu→	|shouldComponentUpdate method
+cwup→	|componentWillUpdate method
+cdup→	|componentDidUpdate method
+cwun→	|componentWillUnmount method
+ren→	|render method
+sst→	|this.setState with object as parameter
+ssf→	|this.setState with function as parameter
+props→	|this.props
+state→	|this.state
+bnd→	|binds the this of method inside the constructor
+
+The following table lists all
